d158ab986f8ede72dc6755e567fb524c5563b99c
Kristoff <chrystalio.k@outlook.com>
2022-07-31 23:14:51 +0700

    Importing JWT Auth and implement on user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,14 @@
 namespace App\Models;
 
 
-use App\Models\Forum;
-use App\Models\ForumComments;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -44,6 +43,26 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     public function forums(){
         return $this->hasMany(Forum::class);
