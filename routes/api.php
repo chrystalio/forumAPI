172640076bacd39e07410314a58420c2faa3df06
chrystalio <chrystalio.k@outlook.com>
2023-02-03 10:44:04 +0700

    Refactor and register forumcontroller on api route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForumController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,9 +23,14 @@ use Illuminate\Support\Facades\Route;
  */
 Route::post('/register', RegisterController::class)->name('register');
 
-Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router){
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::post('/me', [AuthController::class, 'me']);
+Route::group(['middleware' => 'api'], function ($router){
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('me', [AuthController::class, 'me']);
+    });
+
+    Route::apiResource('forums', ForumController::class);
+
 });
