c4509eb18bd68537b5f312ba82ec975c1cd18a67
chrystalio <chrystalio.k@outlook.com>
2023-04-21 21:55:13 +0700

    Replace to Tymon JWTSubcject

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 
-use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
