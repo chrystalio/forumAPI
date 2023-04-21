6873e297e30bbfe62a01726ff3164dee841453fc
chrystalio <chrystalio.k@outlook.com>
2023-04-22 02:37:25 +0700

    Add custom jwt.verify middleware and register on kernel.php

---
+++ app/Http/Middleware/JWTMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JWTMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Invalid token / Authenticated'], 401);
+        }
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+// Set the authenticated user on the request object
+        $request->auth = $user;
+
+        return $next($request);
+    }
+}
