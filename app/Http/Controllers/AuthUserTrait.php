5257abc13fa2baba0c19fd9c4d6073d961cea57c
chrystalio <chrystalio.k@outlook.com>
2023-04-24 01:32:42 +0700

    Refactor AuthUserTrait, add checkOwnership method

--- app/Http/Controllers/AuthUserTrait.php
+++ app/Http/Controllers/AuthUserTrait.php
@@ -1,21 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Exceptions\UserNotDefinedException;
+use Illuminate\Auth\SessionGuard;
 
 trait AuthUserTrait
 {
     public function getAuthUser()
     {
         try {
-            $user = auth()->user();
-        } catch (UserNotDefinedException $e) {
+            return Auth::guard('api')->userOrFail();
+        } catch (AuthenticationException $e) {
             return response()->json([
                 'success' => false,
                 'message' => 'Not authorized, You must login first'
             ], 403);
         }
+    }
 
-        return $user;
+
+    private function checkOwnership($owner): void
+    {
+        $user = $this->getAuthUser();
+
+        if ($user->id !== $owner) {
+            throw new \Exception('You are not authorized to update this post');
+        }
     }
 }
