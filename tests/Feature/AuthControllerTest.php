f970407303fa08f2a34701eddcce92a260e3d00d
Chrystalio <chrystalio.k@outlook.com>
2023-11-19 01:24:09 +0700

    Add User can login Test

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -6,34 +6,6 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 uses(DatabaseTransactions::class);
 
-//test('User can register', function (){
-//    $user = User::factory()->make();
-//
-//    $response = $this->postJson('/api/register', [
-//        'username' => $user->username,
-//        'email' => $user->email,
-//        'password' => 'password',
-//        'password_confirmation' => 'password'
-//    ]);
-//
-//    $response->assertStatus(201);
-//    $response->assertJsonStructure([
-//        'success',
-//        'user' => [
-//            'username',
-//            'email',
-//            'updated_at',
-//            'created_at',
-//            'id'
-//        ]
-//    ]);
-//
-//    $this->assertDatabaseHas('users', [
-//        'username' => $user->username,
-//        'email' => $user->email
-//    ]);
-//});
-
 test('User can register', function () {
     $user = User::factory()->make();
 
@@ -58,4 +30,18 @@ test('User can register', function () {
         'username' => $user->username,
         'email' => $user->email
     ]);
+});
+
+test('User can login', function () {
+    $user = User::factory()->create();
+
+    $this->postJson('/api/auth/login', [
+        'email' => $user->email,
+        'password' => 'password',
+    ])->assertOk()
+        ->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in',
+        ]);
 });
