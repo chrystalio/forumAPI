e249c5144f9fe9ddecf7e49cd21b69999c709cc2
Chrystalio <chrystalio.k@outlook.com>
2023-11-19 17:25:16 +0700

    Add User can get posts

---
+++ tests/Feature/ForumControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Models\Forum;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+uses(DatabaseTransactions::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+    $this->actingAs($this->user);
+});
+
+test('can get a paginated list of forums', function () {
+    $forum = Forum::factory()->create();
+
+    $this->get('/api/forums')
+        ->assertStatus(200)
+        ->assertJsonStructure([
+            'current_page',
+            'data' => [
+                '*' => [
+                    'id',
+                    'title',
+                    'slug',
+                    'body',
+                    'category',
+                    'user_id',
+                    'created_at',
+                    'updated_at',
+                    'user' => [
+                        'id',
+                        'username',
+                    ],
+                ],
+            ],
+            'first_page_url',
+            'from',
+            'last_page',
+            'last_page_url',
+            'links' => [
+                '*' => [
+                    'url',
+                    'label',
+                    'active',
+                ],
+            ],
+            'next_page_url',
+            'path',
+            'per_page',
+            'prev_page_url',
+            'to',
+            'total',
+        ]);
+
+    $this->assertDatabaseHas('forums', [
+        'title' => $forum->title,
+        'slug' => $forum->slug,
+        'body' => $forum->body,
+        'category' => $forum->category,
+        'user_id' => $forum->user_id,
+    ]);
+});
